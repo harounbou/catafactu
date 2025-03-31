5c499f1b66e5fa260d2bb78c7b92b497c87cdf25
harounbou <harounbou@yahoo.co.uk>
2025-04-01 00:26:06 +0100

    more...

--- pos_system/data/query.sql
+++ pos_system/data/query.sql
@@ -1,8 +1,1 @@
-INSERT INTO transactions (
-    transaction_id, items, total_amount, final_amount, transaction_date,
-    deposit_amount, remaining_amount, client_info
-)
-SELECT 
-    id, items, final_amount AS total_amount, final_amount, 
-    date AS transaction_date, deposit_amount, remaining_amount, client_info
-FROM transactions_old;
+.tables
